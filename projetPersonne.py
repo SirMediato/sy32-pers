00e0ab2ebc64852015aeea02c401359bc6d2a6be
SirMediato <SirMediato@users.noreply.github.com>
2017-06-25 11:56:15 +0200

    Update projetPersonne.py

--- projetPersonne.py
+++ projetPersonne.py
@@ -26,24 +26,24 @@ pathTrain = path+"\\train"
 pathTest = path+"\\test"
 
 #Parameters
-heightWindow = 160
+heightWindow = 165
 widthWindow = 70
-nbStepW = 8
-nbStepH = 10
-nbStepWTest = 6
-nbStepHTest = 7
-nbWindows = 2
-rescales = np.arange(1/6,8/6,1/6)
-rescalesTrain = np.arange(1/6,1,1/6)
-rescalesTest = np.arange(1/6,1,1/6)
+nbStepW = 5
+nbStepH = 7
+nbStepWTest = 10
+nbStepHTest = 12
+nbWindows = 1
+rescales = np.arange(1/6,6/6,1/6)
+rescalesTrain = np.arange(1/6,6/6,1/6)
+rescalesTest = np.arange(1/6,9/6,1/6)
 
 #Hog params
-orientation = 8
-pixelPerCell= (10,10)
+orientation = 9
+pixelPerCell= (5,5)
 cellsPerBlock = (1,1)
 #clfParams
-CTrain = 8
-CTest = 8
+CTrain = 5
+CTest = 5
 validCroiseeFolds = 5
 seuilDetect = 0.6
 seuilDetectTest = 0.4
@@ -51,8 +51,7 @@ seuilDetectTest = 0.4
 #Extrait une sous image à partir de l'image passé en paramètre
 def extractWindow(im,x,y):
     window = im[y:(y+heightWindow),x:(x+widthWindow)]
-    window = feature.hog(window, orientations=orientation, pixels_per_cell=pixelPerCell,cells_per_block=cellsPerBlock, visualise=True)[1]
-    window = window.reshape(widthWindow*heightWindow)
+    window = feature.hog(window, orientations=orientation, pixels_per_cell=pixelPerCell,cells_per_block=cellsPerBlock)
     return window
 
 def valid_crois(x,y,clf,nb):
@@ -82,6 +81,97 @@ def aRecouv(bi,bj,seuil):
     else:
         return 0
     
+def writeResults(res):
+    file = open("results.txt","w")
+    for i in np.arange(0,len(res)):
+        num = str(res[i][0])
+        x = str(res[i][1])
+        y = str(res[i][2])
+        w = str(res[i][3])
+        h = str(res[i][4])
+        prob = str(res[i][5])
+        file.write(num+" "+x+" "+y+" "+w+" "+h+" "+prob+"\n")
+    file.close()
+    
+def displayim(pathIm,lab):
+    im = Image.open(pathIm)
+    imdr = ImageDraw.Draw(im)
+    imdr.line([(lab[1],lab[2]),(lab[1]+lab[3],lab[2])], (0,200,255), width=5)
+    imdr.line([(lab[1],lab[2]),(lab[1],lab[2]+lab[4])], (0,200,255), width=5)
+    imdr.line([(lab[1],lab[2]+lab[4]),(lab[1]+lab[3],lab[2]+lab[4])], (0,200,255), width=5)
+    imdr.line([(lab[1]+lab[3],lab[2]),(lab[1]+lab[3],lab[2]+lab[4])], (0,200,255), width=5)
+    im.show()
+
+
+def addFinal(temp,final):
+    temp = sorted(temp,reverse=True,key=lambda x: x[5])
+    k = 1
+    for j in np.arange(1,len(temp)):
+        if(aRecouv(temp[j-k],temp[j],0.5) == 0):
+            final.append(temp[j-k])
+            k=1
+        if(aRecouv(temp[j-k],temp[j],0.5) == 1):
+            k=k+1
+def addFinal2(temp,final):
+    temp = sorted(temp,reverse=True,key=lambda x: x[5])
+    final.append(temp[0])
+    
+def non_max_suppression(boxes, overlapThresh):
+    boxes = np.asarray(boxes)
+    if len(boxes) == 0:
+        return []
+
+    
+    boxes = boxes.astype("float")
+
+    pick = []
+
+    x1 = boxes[:,1]
+    y1 = boxes[:,2]
+    x2 = boxes[:,1]+boxes[:,3]
+    y2 = boxes[:,2]+boxes[:,4]
+
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+ 
+
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        overlap = (w * h) / area[idxs[:last]]
+ 
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],np.where(overlap > overlapThresh)[0])))
+ 
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    retList = []
+    for i in pick:
+        retList.append([str(int(boxes[i,0])).zfill(3),int(boxes[i,1]),int(boxes[i,2]),int(boxes[i,3]),int(boxes[i,4]),boxes[i,5]])
+    return retList
+    
+
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
 files = os.listdir(pathTrain)
 filesTest = os.listdir(pathTest)
 vect = np.zeros((len(files),5))
@@ -92,11 +182,15 @@ with open(path+'\\label.txt') as f:
        i=i+1
 vect = vect.astype(int)
 n = len(files)*nbWindows*len(rescales)+len(files)
-windows = np.zeros((n,heightWindow*widthWindow))
+splitWin = (math.floor(widthWindow/pixelPerCell[0]),math.floor(heightWindow/pixelPerCell[1]))
+sizeWindow = splitWin[0]*splitWin[1]*orientation
+windows = np.zeros((n,sizeWindow))
 label = np.concatenate((-1*np.ones((len(files)*nbWindows*len(rescales))),1*np.ones(len(files))),axis=0)
+
+
+#On créé et stocke des fenetres de taille différente aléatoires pour faire des exemples négatifs et positifs
 i = 0
 j = 0
-#On créé et stocke des fenetres de taille différente aléatoires pour faire des exemples négatifs et positifs
 for f in files:
     img = util.img_as_float(color.rgb2gray(io.imread(pathTrain + "\\" + f)))  
     for rc in rescales:
@@ -112,8 +206,8 @@ for f in files:
     currLabel = vect[j]
     window = img[currLabel[2]:currLabel[2]+currLabel[4],currLabel[1]:currLabel[1]+currLabel[3]]
     window = transform.resize(window,(heightWindow,widthWindow),mode='constant' ,order=0)
-    window = feature.hog(window, orientations=orientation, pixels_per_cell=pixelPerCell,cells_per_block=cellsPerBlock, visualise=True)[1]
-    windows[j+len(files)*nbWindows*len(rescales)] = window.reshape(widthWindow*heightWindow)
+    window = feature.hog(window, orientations=orientation, pixels_per_cell=pixelPerCell,cells_per_block=cellsPerBlock)
+    windows[j+len(files)*nbWindows*len(rescales)] = window
     j=j+1
 
 print("First windows done")
@@ -160,7 +254,7 @@ for f in files:
                     labelRescaled = rc * currLabel
                     airCommun = abs(X+widthWindow-labelRescaled[0]) * abs(Y+heightWindow-labelRescaled[1])
                     label2 = [f.split('.')[0],X,Y,widthWindow,heightWindow]
-                    if(aRecouv(label2,labelRescaled,0.5) == 0):
+                    if(aRecouv(label2,labelRescaled,0.6) == 0):
                         fakeWindows.append(window[0])
                         #filenum = f.split('.')[0]
                         #results.append([filenum,math.floor(X/rc),math.floor(Y/rc),math.floor(widthWindow/rc),math.floor(heightWindow/rc)])
@@ -179,7 +273,6 @@ n_samples = windows.shape[0]
 #cross_val_score(clf, windows, label, cv=cv)
 windows, label = shuffle(windows,label, random_state=0)
 taux2 = valid_crois(windows,label,clfTest,validCroiseeFolds)
-clf.fit(windows,label)
 
 
 
@@ -188,8 +281,10 @@ print("Error rate second training :")
 print(taux2)
 print("Starting detection on test images")
 
-#Prédictions positives sur les images de test
 results = []
+#Prédictions positives sur les images de test
+
+    
 for f in filesTest:
     img = util.img_as_float(color.rgb2gray(io.imread(pathTest + "\\" + f)))
     print(f)
@@ -208,8 +303,8 @@ for f in filesTest:
             for Y in np.arange(0,imgrs.shape[0]-heightWindow,stepY):
                 window = [extractWindow(imgrs,X,Y)]
                 predict = clfTest.predict_proba(window)[0,1]
-                pred = clfTest.predict(window)
-                if pred == 1:
+                #pred = clfTest.predict(window)
+                if predict >= 0.08:
                     filenum = f.split('.')[0]
                     results.append([filenum,math.floor(X/rc),math.floor(Y/rc),math.floor(widthWindow/rc),math.floor(heightWindow/rc),predict])
 
@@ -217,35 +312,13 @@ print("Detection done !")
 
 
 
-def displayim(pathIm,lab):
-    im = Image.open(pathIm)
-    imdr = ImageDraw.Draw(im)
-    imdr.line([(lab[1],lab[2]),(lab[1]+lab[3],lab[2])], (0,200,255), width=5)
-    imdr.line([(lab[1],lab[2]),(lab[1],lab[2]+lab[4])], (0,200,255), width=5)
-    imdr.line([(lab[1],lab[2]+lab[4]),(lab[1]+lab[3],lab[2]+lab[4])], (0,200,255), width=5)
-    imdr.line([(lab[1]+lab[3],lab[2]),(lab[1]+lab[3],lab[2]+lab[4])], (0,200,255), width=5)
-    im.show()
-"""
-for i in np.arange(0,len(results)):
-    displayim(pathTest+'\\'+results[i][0]+".jpg",results[i])
-"""
 
-def addFinal(temp,final):
-    temp = sorted(temp,reverse=True,key=lambda x: x[5])
-    k = 1
-    for j in np.arange(1,len(temp)):
-        if(aRecouv(temp[j-k],temp[j],0.5) == 0):
-            final.append(temp[j-k])
-            k=1
-        if(aRecouv(temp[j-k],temp[j],0.5) == 1):
-            k=k+1
-def addFinal2(temp,final):
-    temp = sorted(temp,reverse=True,key=lambda x: x[5])
-    final.append(temp[0])
-    
+
+
 currFile = ""
 temp = []
-finalresults = []
+
+
 finalresults2 = []
 for i in np.arange(0,len(results)):
     if(currFile == results[i][0]):
@@ -264,5 +337,62 @@ for i in np.arange(0,len(results)):
         elif(len(temp)!=0):
             addFinal2(temp,finalresults2)
 
+
 for i in np.arange(0,len(finalresults2)):
     displayim(pathTest+'\\'+finalresults2[i][0]+".jpg",finalresults2[i])
+    
+writeResults(finalresults2)
+
+"""
+currFile = ""
+temp = []
+
+finalresults = []
+for i in np.arange(0,len(results)):
+    if(currFile == results[i][0]):
+        temp.append(results[i])
+    elif(currFile != results[i][0]):
+        if(len(temp)==1):
+            finalresults.append(temp[0])
+        elif(len(temp)!=0):
+            addFinal(temp,finalresults)
+        temp=[]
+        currFile=results[i][0]
+        temp.append(results[i])
+    if(i==len(results)-1):
+        if(len(temp)==1):
+            finalresults.append(temp[0])
+        elif(len(temp)!=0):
+            addFinal(temp,finalresults)
+            
+for i in np.arange(0,len(finalresults)):
+    displayim(pathTest+'\\'+finalresults[i][0]+".jpg",finalresults[i])
+
+
+writeResults(finalresults)
+
+currFile = ""
+temp = []
+
+nmfinalresults = []
+for i in np.arange(0,len(results)):
+    if(currFile == results[i][0]):
+        temp.append(results[i])
+    elif(currFile != results[i][0]):
+        nm_res = non_max_suppression(temp,0.5)
+        for j in np.arange(0,len(nm_res)):
+            nmfinalresults.append(nm_res[j])
+        temp=[]
+        currFile=results[i][0]
+        temp.append(results[i])
+    if(i==len(results)-1):
+        nm_res = non_max_suppression(temp,0.5)
+        for j in np.arange(0,len(nm_res)):
+            nmfinalresults.append(nm_res[j])
+            
+for i in np.arange(0,len(nmfinalresults)):
+    displayim(pathTest+'\\'+nmfinalresults[i][0]+".jpg",nmfinalresults[i])
+
+
+writeResults(nmfinalresults)
+"""
